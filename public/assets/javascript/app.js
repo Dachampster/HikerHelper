ad2b13e748f570fcdee84bff3b472b4bcf0b088c
bpzimmerman <32779243+bpzimmerman@users.noreply.github.com>
2018-04-12 11:40:18 -0400

    merged app.js and app2.js

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,5 +1,5 @@
 
- $(document).ready(function() {
+$(document).ready(function() {
   var address = "";
   var searchLat = "";
   var searchLon = "";
@@ -7,148 +7,107 @@
   var searchDiff;
   var searchRadius;
   var currentSearchesArray = [];
+
+  // click to submit the search request
+  $("#searchButton").on("click",function(){
+    event.preventDefault();
+
+    // clear any previously submitted searches
+    currentSearchesArray = [];
+    $("#hikingDiv").empty();
+
+    // get information from search form
+    address = $("#inlineFormInput").val().trim();
+    searchLength = parseFloat($("#search-min-lng").val().trim());
+    searchDiff = $("#search-diff").val();
+    searchRadius = parseFloat($("#search-max-dist").val().trim());
+
+    // set trail length and search radius defaults if a number is not entered
+    if (isNaN(searchLength)){
+      searchLength = 0;
+    };
+    if (isNaN(searchRadius)){
+      searchRadius = 30;
+    };
+
+    // create object containing search information
+    var searchInfo = {
+      address: address,
+      searchLength: searchLength,
+      searchRadius: searchRadius
+    };
  
-   $("#searchButton").on("click",function(){
-     event.preventDefault();
-     currentSearchesArray = [];
-     $("#hikingDiv").empty();
+    // call function to get the trails based on the serach information entered
+    findNewTrails(searchInfo);
+
+    $("#inlineFormInput").val("");
+
+    setTimeout(function(){ location.href= '#hikingDiv'; }, 500);
    
+  });
 
-     
-   //   //  eventName= $("#eventName-input").val().trim();
-        address = $("#inlineFormInput").val().trim();
-   //   //  city = $("#city-input").val().trim();
-   //   //  state = $("#state-input").val().trim();
-        searchLength = parseFloat($("#search-min-lng").val().trim());
-        searchDiff = $("#search-diff").val();
-        searchRadius = parseFloat($("#search-max-dist").val().trim());
-        if (isNaN(searchLength)){
-          searchLength = 0;
-        };
-        if (isNaN(searchRadius)){
-          searchRadius = 30;
-        };
- 
-   // searchAndAdd();
- findCoordinates();
-   //   // $("#eventName-input").val("");
-       $("#inlineFormInput").val("");
-   //   // $("#city-input").val("");
-   //   // $("#state-input").val("");
+  function findNewTrails(data){
 
-   setTimeout(function(){ location.href= '#hikingDiv'; }, 500);
-   
- });
+    $.get("/api/ex/trail", data, function(response){
+      console.log(response);
+      searchLat = response.location.lat;
+      searchLon = response.location.lng;
 
- 
- 
- 
-   function findCoordinates(){
-   
-      var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address +"&key=AIzaSyCWa5eHnMAMi6rkFWh1pg_Ssxz8lTN6lQk";
-  
-     $.ajax({
-         url: queryURL,
-         method: "GET",
-         async: true
-     }).done(function(response)
-     {
-       
-       searchLat = response.results[0].geometry.location.lat;
-       searchLon = response.results[0].geometry.location.lng;
- 
-       var formatLocation = {
-         lat:searchLat ,
-         lon: searchLon 
-       };
+      $("#hikingDiv").attr("data-length", searchLength)
+                     .attr("data-difficulty", searchDiff)
+                     .attr("data-radius", searchRadius)
+                     .attr("data-lat", searchLat)
+                     .attr("data-lng", searchLon);
 
-       $("#hikingDiv").attr("data-length", searchLength)
-                      .attr("data-difficulty", searchDiff)
-                      .attr("data-radius", searchRadius)
-                      .attr("data-lat", searchLat)
-                      .attr("data-lng", searchLon);
-
-       hikingSearch(searchLat,searchLon);
+      var pullID = 0;
+      if(response.trails.length == 0){
+        console.log("No trails found");
+      };
       
-      });
-     } //function findCoordinates ends
- 
-     function hikingSearch(hikingLat,hikingLon){
-       
-          
-          var hikeSearch = {
-              hikingApiKey : "200242829-ff1de9f4eecd59e41080ee24ed53c7ed",
-              lat: hikingLat,
-              lon:hikingLon,
-          };
-          
-          
-          
-          var queryUrl = "https://www.hikingproject.com/data/get-trails?lat="+hikeSearch.lat+"&lon="+hikeSearch.lon+"&maxDistance="+searchRadius+"&minLength="+searchLength+"&key=" + hikeSearch.hikingApiKey;
-          
-          $.ajax({
-            url: queryUrl,
-            method: "GET",
-            async: true
-        }).done(function(response)
-        {
-          
-          
-          var pullID = 0;
-          if(response.trails.length == 0){
-            console.log("No trails found");
-          };
-          
-          var trailInfo = {};
-          for(var i = 0; i < response.trails.length; i++){
-          // console.log(response.trails[i]);
-            trailInfo = response.trails[i];
-          // pushes respones to array so that more info can be displayed to user in modal
-          currentSearchesArray.push(trailInfo);
-          
-            var difficulty = trailInfo.difficulty;
-            var trailName = trailInfo.name;
-    
-          //   <div class="card border-primary mb-3" style="max-width: 20rem;">
-          //     <div class="card-header">Header</div>
-          //     <div class="card-body">
-          //       <h4 class="card-title">Primary card title</h4>
-          //       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          //     </div>
-          //  </div>
-    
-            var newDiv = $("<div data-toggle='modal' data-target='#moreInfo-Modal'>");
-            newDiv.attr("data-actNum", trailInfo.id)
-                  .attr("data-actName", trailInfo.name)
-                  .attr("data-actDiff", trailInfo.difficulty)
-                  .attr("data-actLength", trailInfo.length)
-                  .attr("data-actRating", trailInfo.stars)
-                  .attr("data-lat", trailInfo.latitude)
-                  .attr("data-long", trailInfo.longitude);
-            var newIMG = $("<img>");
-            var trailIMG = trailInfo.imgSmall.replace(/\\\//g, "/");
-            if (trailIMG == '')
-            trailIMG = "http://via.placeholder.com/240x180"
-            newIMG.attr("src", trailIMG);
-            
-            newDiv.attr('data-pullID', pullID++);
-            
-            var newP = $("<p>");
-            var newH = $("<h5>");
-            newH.append( trailInfo.name);
-            newP.append(trailInfo.location);
-            
-            // need to append to the div in html
-            newDiv.append(newH);
-            newDiv.append(newP);
-            newDiv.append(newIMG);
-            newDiv.addClass("trails");
-            
-            $("#hikingDiv").append(newDiv);
-          }
-          console.log(currentSearchesArray);
-          });
-       }//hikingSearch ends
+      var trailInfo = {};
+      for(var i = 0; i < response.trails.length; i++){
+      
+        trailInfo = response.trails[i];
+        // pushes response to array so that more info can be displayed to user in modal
+        currentSearchesArray.push(trailInfo);
+      
+        var difficulty = trailInfo.difficulty;
+        var trailName = trailInfo.name;
+
+        var newDiv = $("<div data-toggle='modal' data-target='#moreInfo-Modal'>");
+        newDiv.attr("data-actNum", trailInfo.id)
+              .attr("data-actName", trailInfo.name)
+              .attr("data-actDiff", trailInfo.difficulty)
+              .attr("data-actLength", trailInfo.length)
+              .attr("data-actRating", trailInfo.stars)
+              .attr("data-lat", trailInfo.latitude)
+              .attr("data-lng", trailInfo.longitude);
+        var newIMG = $("<img>");
+        var trailIMG = trailInfo.imgSmall.replace(/\\\//g, "/");
+        if (trailIMG == ''){
+          trailIMG = "http://via.placeholder.com/240x180"
+        };
+        newIMG.attr("src", trailIMG);
+        
+        newDiv.attr('data-pullID', pullID++);
+        
+        var newP = $("<p>");
+        var newH = $("<h5>");
+        newH.append( trailInfo.name);
+        newP.append(trailInfo.location);
+        
+        // need to append to the div in html
+        newDiv.append(newH);
+        newDiv.append(newP);
+        newDiv.append(newIMG);
+        newDiv.addClass("trails");
+        
+        $("#hikingDiv").append(newDiv);
+      }
+      console.log(currentSearchesArray);
+    });
+   
+  };
 
        function airportSearch(lat, lng){
         googlePlacesSearch = {
@@ -184,8 +143,10 @@
           var thisIndex = $(this).attr("data-pullid");
           var thisTrail = currentSearchesArray[thisIndex];
           var trailIMG = thisTrail.imgSmallMed.replace(/\\\//g, "/");
-          if(trailIMG == "")
+
+          if(trailIMG == ""){
             trailIMG =  "http://via.placeholder.com/340x250";
+          };
           
           $("#content-title").text(thisTrail.name);
           $(".modal-body img").attr("src", trailIMG)
@@ -194,9 +155,11 @@
                               .attr("data-actDiff", $(this).attr("data-actDiff"))
                               .attr("data-actLength", $(this).attr("data-actLength"))
                               .attr("data-actRating", $(this).attr("data-actRating"))
+                              .attr("data-actLat", $(this).attr("data-lat"))
+                              .attr("data-actLng", $(this).attr("data-lng"));
           $(".modal-body p").empty();
           $(".modal-body p").append("<p>"+thisTrail.location+"</p>");
-          $(".modal-body p").append("<p>Current Condition Details: " + thisTrail.conditionDetails +"</p>");
+          $(".modal-body p").append("<p>Current Condition Details: " + thisTrail.conditionDetails + "</p>");
           
        })
  
